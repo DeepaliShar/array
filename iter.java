8282c03a37e626cca09d112bca1b5eb1ebe1381f
DeepaliShar <212deepali.sharma@gmail.com>
2023-03-26 11:37:31 +0530

    Done

--- iter.java
+++ iter.java
@@ -1,7 +1,7 @@
 import java.util.*;
 class iter{
     public static void main(String[] args) {
-        int a[] ={1,2,3,4,4};
+        int a[] ={1,2,3,4,5,5,5,56,4,4};
 
         for(int x : a)
         System.out.println(x);
